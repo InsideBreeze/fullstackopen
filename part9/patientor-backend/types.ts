d96e69a0e3347ebd030badc42b852e27de148667
zephyr <linjie.zhang@outlook.com>
2023-02-11 21:33:38 +0800

    fix

--- part9/patientor-backend/types.ts
+++ part9/patientor-backend/types.ts
@@ -4,8 +4,7 @@ export interface Diagnose {
   latin?: string;
 }
 
-// eslint-disable-next-line
-export interface Entry {}
+
 export interface Patient {
   id: string;
   name: string;
@@ -17,11 +16,55 @@ export interface Patient {
 }
 
 export enum Gender {
-  MALE = "male",
-  FEMALE = "female",
-  OTHHER = "other",
+  Male = "male",
+  Female = "female",
+  Other = "other",
 }
 
 export type NonSensitivePatient = Omit<Patient, "ssn" | "entries">;
 
 export type NewPatient = Omit<Patient, "id">;
+
+interface BaseEntry {
+    id: string;
+    description: string;
+    date: string;
+    specialist: string;
+    diagnosisCodes?: Array<Diagnose["code"]>;
+}
+
+export enum HealthCheckRating {
+    "Healthy" = 0,
+    "LowRisk" = 1,
+    "HighRisk" = 2,
+    "CriticalRisk" = 3
+}
+
+interface HealthCheckEntry extends BaseEntry {
+    type: "HealthCheck";
+    healthCheckRating: HealthCheckRating
+}
+
+interface HospitalEntry extends BaseEntry {
+    type: "Hospital";
+    discharge: Discharge
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+    employerName: string;
+    sickLeave?: SickLeave
+}
+interface Discharge {
+    date: string;
+    criteria: string
+}
+
+interface SickLeave {
+    startDate: string;
+    endDate: string
+}
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
