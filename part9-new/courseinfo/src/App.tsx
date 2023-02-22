e82f46041fa69772f4bb04fe71f44708454cebc4
zephyr <linjie.zhang@outlook.com>
2023-02-22 19:52:39 +0800

    9.14

---
+++ part9-new/courseinfo/src/App.tsx
@@ -0,0 +1,56 @@
+const App = () => {
+  const courseName = "Half Stack application development";
+  const courseParts = [
+    {
+      name: "Fundamentals",
+      exerciseCount: 10,
+    },
+    {
+      name: "Using props to pass data",
+      exerciseCount: 7,
+    },
+    {
+      name: "Deeper type usage",
+      exerciseCount: 14,
+    },
+  ];
+
+  return (
+    <div>
+      <Header name={courseName} />
+      <Content parts={courseParts} />
+      <Total
+        total={courseParts.reduce(
+          (carry, part) => carry + part.exerciseCount,
+          0
+        )}
+      />
+    </div>
+  );
+};
+
+const Header = ({ name }: { name: string }) => {
+  return <h1>{name}</h1>;
+};
+
+type Part = {
+  name: string;
+  exerciseCount: number;
+};
+
+const Content = ({ parts }: { parts: Part[] }) => {
+  return (
+    <>
+      {parts.map((part) => (
+        <p key={part.name}>
+          {part.name} {part.exerciseCount}
+        </p>
+      ))}
+    </>
+  );
+};
+
+const Total = ({ total }: { total: number }) => {
+  return <p>Number of exercises {total}</p>;
+};
+export default App;
